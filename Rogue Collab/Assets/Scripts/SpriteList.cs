3e33ace214035868e5c0811c8884cbb78c7b2959
Julian <juliathouse@aol.com>
2017-12-01 18:27:45 +0100

    Player Sprites hinzugefügt
    Jetzt ist es Kunst!

---
+++ Rogue Collab/Assets/Scripts/SpriteList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteList : Singleton<SpriteList> {
+
+   public Sprite PlayerFront;
+   public Sprite PlayerBack;
+   public Sprite PlayerLeft;
+   public Sprite PlayerRight;
+}
