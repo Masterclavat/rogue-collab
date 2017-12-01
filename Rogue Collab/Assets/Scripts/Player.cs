cef64cce061deb23c4a0ea1f81abb9845c1b08fc
Julian <juliathouse@aol.com>
2017-12-01 16:30:27 +0100

    Zeilenenden
    Zeilenenden auf crlf geändert

--- Rogue Collab/Assets/Scripts/Player.cs
+++ Rogue Collab/Assets/Scripts/Player.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+   void Start() {
+
+   }
+
+   void Update() {
+
+   }
 }
