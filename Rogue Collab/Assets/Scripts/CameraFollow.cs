1ed949fe22544b059e0843d918a929ef8fcbb9aa
clavatmaster <10CEDRIC10.cn@gmail.com>
2017-12-01 17:25:17 +0100

    CameraFollow implementiert

---
+++ Rogue Collab/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+   public Transform FollowTransform;
+   private bool follow;
+   private void Start() {
+
+   }
+
+   private void LateUpdate() {
+      if (!follow || FollowTransform == null) {
+         return;
+      }
+      transform.position = FollowTransform.position;
+   }
+
+   public void StartFollow() {
+      follow = true;
+   }
+
+   public void StopFollow() {
+      follow = false;
+   }
+}
--- Rogue Collab/Assets/Scripts/Player.cs
+++ Rogue Collab/Assets/Scripts/Player.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-   public float Speed;
-   public float Damage;
 
    void Start() {
 
